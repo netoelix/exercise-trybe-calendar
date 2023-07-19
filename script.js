d3954de13e2a7b5ed8f33feb74a228650b2fd182
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-18 21:49:08 -0300

    Adicionando os dias da semana e suas respectivas paginas

--- script.js
+++ script.js
@@ -17,3 +17,34 @@ createDaysOfTheWeek();
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 // Escreva seu código abaixo.
+
+const creatDaysOfDecember = () => {
+  for (let index = 0; index < decemberDaysList.length; index +=1) {
+    let createDays = document.createElement('li');
+    createDays.id = 'days';
+    createDays.innerHTML = decemberDaysList[index];
+    let parent = document.getElementById('days');
+    parent.appendChild(createDays);
+    switch (decemberDaysList[index]) {
+      case 24:
+        createDays.id = 'day holiday';
+      break;
+      case 25:
+        createDays.id = 'day holiday friday';
+      break;
+      case 31:
+        createDays.id = 'day holiday';
+      break;
+      case 4:
+        createDays.id = 'day friday';
+      break;
+      case 11:
+        createDays.id = 'day friday';
+      break;
+      case 18:
+        createDays.id = 'day friday';
+      break;
+    }
+  }
+}
+creatDaysOfDecember();
